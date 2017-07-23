8f4bd16cd32aaf0821ea6bbfebca9c3bde4aa7f8
chidambarampriya <30342779+chidambarampriya@users.noreply.github.com>
2017-07-23 14:44:02 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num,n=40,m=20;
+printf("print odd numbers in the given range n to m:\n");
+for(num=m;num<=n;num++)
+{
+if(num%2==1)
+{
+printf(%d",num);
+}
+}
+}
