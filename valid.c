1dbb3aaea07bf3eb71657acff271c0ed9f61660c
chidambarampriya <30342779+chidambarampriya@users.noreply.github.com>
2017-08-04 16:47:31 +0530

    Create valid.c

---
+++ valid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define MIN 0
+#define MAX 9 
+void main()
+{
+    int n;
+    while (1) 
+    {
+        printf("Enter a number (%d-%d) :", MIN, MAX);
+        scanf("%d", &n);
+     if (n >= MIN && n <= MAX) 
+     {
+         printf("Good\n");
+     } else 
+     {
+         printf("Damn you!\n");
+         break;
+      }
+    }
+}
