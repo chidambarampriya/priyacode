0a1360eb32c8f8cf67199ab71f42d5214b2d2eb6
chidambarampriya <30342779+chidambarampriya@users.noreply.github.com>
2017-08-05 16:30:07 +0530

    Update table.c

--- table.c
+++ table.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-int main()
+void main()
 {
     int n, i;
-
     printf("Enter an integer: ");
     scanf("%d",&n);
-
     for(i=1; i<=10; ++i)
     {
         printf("%d * %d = %d \n", n, i, n*i);
     }
     
-    return 0;
 }
